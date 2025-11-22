5d063c64864d66127a0a9e3f65ef414915d7e2eb
adit1080 <adit1080army@gmail.com>
2025-11-22 11:18:21 +0200

    QuestionLevel Enum was added
    QuestionLevel Enum was added to provide distinction to different levels of questions (like easy hard etc)

---
+++ src/model/QuestionLevel.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * Enumeration of difficulty levels for questions.
+ * Separate from game difficulty.
+ */
+public enum QuestionLevel {
+    EASY,
+    MEDIUM,
+    HARD,
+    MASTER;
+}
+
