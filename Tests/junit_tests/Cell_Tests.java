66756d442f10d161478f66f0b95be50904f43b9a
reychely <racheljust8@gmail.com>
2025-12-11 12:27:39 +0200

    Added Test 2 to Junit
    Adding Junit test for checking the num of lives after the mine was revealed

--- Tests/junit_tests/Cell_Tests.java
+++ Tests/junit_tests/Cell_Tests.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.*;   // JUNIT 4
 public class Cell_Tests {
 
     @Test
+    // checks the number of correct number of points for flag adding
     public void testCorrectFlagAddsPoint() {
 
         Match m = new Match(new Player("A"), new Player("B"), DifficultyLevel.EASY);
@@ -22,5 +23,22 @@ public class Cell_Tests {
         mc.toggleFlag(0, 0, 0);
 
         assertEquals(before + 1, m.points());
+    }
+    
+    @Test
+    //checks the number of lives removed after the mine was opened
+    public void testRevealMineLosesOneLife() {
+
+        Match m = new Match(new Player("A"), new Player("B"), DifficultyLevel.EASY);
+        Board board = m.board1();
+        board.setCellForTest(1, 1, new MineCell());
+
+        MatchController mc = MatchController.getInstance();
+        mc.init(m, SysData.getInstance(), AppController.getInstance());
+
+        int before = m.lives();
+        mc.reveal(1, 1);
+
+        assertEquals(before - 1, m.lives());
     }
 }
