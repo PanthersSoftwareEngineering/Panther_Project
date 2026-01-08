dcba532f269c4a4020537443617ce125aab13108
maorbenyehuda <240499757+PanthersSoftwareEngineering@users.noreply.github.com>
2026-01-08 23:18:03 +0200

    adding music

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,10 @@
 package app;
 import controller.AppController;
+import util.BackgroundMusic;
 
 public class Main {
     public static void main(String[] args) {
+        BackgroundMusic.start();
         AppController.getInstance().showMainMenu();
     }
 }
